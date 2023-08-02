3a9dc0f219c34e309a8d6795e63bb2ef11e9aac4
@The Jack <abhishekdumaniya196@gmail.com>
2023-08-02 23:22:43 +0530

    Basic of the array

---
+++ Array/Basic_array.java
@@ -0,0 +1,12 @@
+package com.abhishek.Array;
+
+public class Basic_array {
+    public static void main(String[] args) {
+        // Syntax
+        // datatype [] variable name = new datatype [size];
+        int [] marks = {5,5,8,5};
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
