731c3519ebdb4417a241a84cd1eac2f5a8120a62
@The Jack <abhishekdumaniya196@gmail.com>
2023-08-07 22:09:18 +0530

    Input Scanner

---
+++ Input.java
@@ -0,0 +1,13 @@
+package com.abhishek;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a your rollnumber: ");
+        int rollnumber = sc.nextInt();
+        System.out.println("Your rollnumber is a: " + rollnumber );
+
+    }
+}
