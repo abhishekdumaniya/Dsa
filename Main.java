2ac9de771b2a48699953b7dd15aedd6d4ceab223
@The Jack <abhishekdumaniya196@gmail.com>
2023-09-16 22:53:53 +0530

    Main file add

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.abhishek;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("Hello World!");
+        System.out.println("My name is a abhishek");
+    }
+}
