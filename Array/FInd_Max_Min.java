558351c23dd657f2f682784c65ff517dcb4b9a5e
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2024-05-27 22:35:07 +0530

    add file

---
+++ Array/FInd_Max_Min.java
@@ -0,0 +1,11 @@
+package com.abhishek.Array;
+
+public class FInd_Max_Min {
+    public static void main(String[] args) {
+    int[] arr  = {4,5,8,9,1,52,5,8,5};
+//    arr = Integer.MAX_VALUE;
+    int y = Integer.MIN_VALUE;
+//        System.out.println(n);
+        System.out.println(y);
+    }
+}
