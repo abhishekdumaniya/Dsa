4734dee6aae66d07164e37f72250abf79952147b
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2024-06-17 14:42:57 +0530

    DLL main file added

--- Linked_List/DLL_main.java
+++ Linked_List/DLL_main.java
@@ -1,2 +1,22 @@
-package com.abhishek.Linked_List;public class DLL_main {
+package com.abhishek.Linked_List;
+public class DLL_main {
+    public static void main(String[] args) {
+        DLL dl = new DLL();
+
+        dl.insertfirst(4);
+        dl.insertfirst(3);
+        dl.insertfirst(2);
+        dl.insertfirst(1);
+        dl.display();
+        System.out.println();
+        System.out.println("Insert the Last Position");
+        dl.insertlast(45);
+        dl.display();
+        System.out.println();
+        System.out.println("Insert the middle");
+        dl.insertmiddle(3,69);
+        dl.display();
+        System.out.println();
+    }
+
 }
